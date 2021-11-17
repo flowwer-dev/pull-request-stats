85ba5022060ff8c40c13ebd4c798789782ed252c
Daniel Cañada García <d4nicoder@gmail.com>
2021-11-18 00:26:30 +0100

    feat: filtering by users only if a list of reviewers is present.

--- src/interactors/getReviewers/index.js
+++ src/interactors/getReviewers/index.js
@@ -3,8 +3,10 @@ const groupReviews = require('./groupReviews');
 const core = require('@actions/core');
 const reviewers = core.getInput('reviewers').split(',')
 
+console.log(`Reviewers: ${reviewers.join(', ')}`)
+
 module.exports = (pulls) => groupReviews(pulls).map(({ author, reviews }) => {
   const stats = calculateReviewsStats(reviews);
   console.log(author)
   return { author, reviews, stats };
-}).filter(info => reviewers.includes(info.author));
+}).filter(info => reviewers.length && reviewers.includes(info.author.login));
