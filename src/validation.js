f25cb4d38868c45844722d3a5b1f808ad1b4506a
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2021-11-20 01:31:03 +0100

    fix: unable to run in `pull_request_target` event
    `pull_request_target` is a sub variant of `pull_request` that provides all the same data and should be supported too.
    
    This fixes being unable to use this action in a secure environment for *forked* pull requests

--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,11 @@
-const VALID_EVENT_NAME = 'pull_request';
+const VALID_EVENT_NAMES = ['pull_request','pull_request_target'];
 
 const validateEnv = (github) => {
   const { eventName } = github.context;
-  if (eventName === VALID_EVENT_NAME) return;
-  const error = `This action runs only in the "${VALID_EVENT_NAME}" event. Change the property "on" of your workflow file from "${eventName}" to "${VALID_EVENT_NAME}".`;
+  for(let validEvent of VALID_EVENT_NAMES){
+    if(eventName === validEvent) return;
+  }
+  const error = `This action runs only in one of the following events "${VALID_EVENT_NAMES.join()}". Change the property "on" of your workflow file from "${eventName}" one of "${VALID_EVENT_NAME.join()}".`;
   throw new Error(error);
 };
 
