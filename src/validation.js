9d0a1b8b388ad62879a80724bd956fe430a69f68
Nils Brugger <nilsbrugger.nb.nb.nb@gmail.com>
2021-11-21 15:57:28 +0100

    style: add missing whitespace
    Co-authored-by: Manuel de la Torre <manuelmhtr@gmail.com>

--- src/validation.js
+++ src/validation.js
@@ -3,7 +3,7 @@ const VALID_EVENT_NAMES = ['pull_request', 'pull_request_target'];
 const validateEnv = (github) => {
   const { eventName } = github.context;
   for (let validEvent of VALID_EVENT_NAMES) {
-    if(eventName === validEvent) return;
+    if (eventName === validEvent) return;
   }
   const error = `This action runs only in one of the following events "${VALID_EVENT_NAMES.join()}". Change the property "on" of your workflow file from "${eventName}" one of "${VALID_EVENT_NAME.join()}".`;
   throw new Error(error);
